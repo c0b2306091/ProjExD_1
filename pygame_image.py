93c8170ffe699fdec390f8b4db631edbc3acf253
栗原潤 <jun@kuriharajunnoMacBook-Air.local>
2024-04-16 16:32:01 +0900

    背景（ワープあり）

--- pygame_image.py
+++ pygame_image.py
@@ -18,11 +18,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-(tmr%800), 0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+        
 
 
 if __name__ == "__main__":
