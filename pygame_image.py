c3c12e400e4866d6cf0c83affc4b62e447cf89e3
栗原潤 <jun@kuriharajunnoMacBook-Air.local>
2024-04-16 17:25:12 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,35 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    Fscreen = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
+
     tmr = 0
     while True:
+        tmr += 1
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,+1)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1,0)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
 
-        screen.blit(bg_img, [-(tmr%800), 0])
-        screen.blit(kk_img,[300,200])
-        pg.display.update()
-        tmr += 1        
+
+        screen.blit(bg_img, [-(tmr%3200), 0])
+        screen.blit(Fscreen, [-(tmr%3200)+1600, 0])
+        screen.blit(bg_img, [-(tmr%3200)+3200, 0])
+        screen.blit(Fscreen, [-(tmr%3200)+4800, 0])
+        screen.blit(kk_img,kk_rct)
+        pg.display.update()        
         clock.tick(200)
         
 
