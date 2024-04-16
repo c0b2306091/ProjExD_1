f1cc124d535e50e88d0bffea7076514c67c6a760
栗原潤 <jun@kuriharajunnoMacBook-Air.local>
2024-04-16 18:05:40 +0900

    演習課題2

--- pygame_image.py
+++ pygame_image.py
@@ -16,6 +16,8 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300,200
+    x=0
+    y=0
 
     tmr = 0
     while True:
@@ -24,16 +26,18 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        x=0
+        y=0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
+            y=-1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0,+1)
+            y=1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2,0)
+            x=2
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1,0)
-
-
+            x=-1
+        kk_rct.move_ip(x,y)
+        
         screen.blit(bg_img, [-(tmr%3200), 0])
         screen.blit(Fscreen, [-(tmr%3200)+1600, 0])
         screen.blit(bg_img, [-(tmr%3200)+3200, 0])
